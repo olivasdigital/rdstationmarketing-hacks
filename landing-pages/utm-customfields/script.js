884b4b9d90551812059bc130974adadf5fe95bc1
Olivas Digital <41170138+olivasdigital@users.noreply.github.com>
2022-02-11 08:40:57 +0100

    Create script.js

---
+++ landing-pages/utm-customfields/script.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+    	setTimeout(function() {
+		        var endereco_da_pagina = window.location.href;
+            var url = new URL(endereco_da_pagina);
+
+            // Popula formulário com campos ocultos repassando as UTMs recebidas
+
+            // UTM source
+            var utm_source = url.searchParams.get('utm_source');
+            if (utm_source != null && utm_source != 'null')
+                $('#conversion-form').append('<input type="hidden" name="utm_source" value="'+ utm_source +'"></input>');
+            
+            // UTM source
+            var utm_medium = url.searchParams.get('utm_medium');
+            if (utm_medium != null && utm_medium != 'null')
+                $('#conversion-form').append('<input type="hidden" name="utm_medium" value="'+ utm_medium +'"></input>');
+
+            // UTM campaign
+            var utm_campaign = url.searchParams.get('utm_campaign');
+            if (utm_campaign != null && utm_campaign != 'null')
+                $('#conversion-form').append('<input type="hidden" name="utm_campaign" value="'+ utm_campaign +'"></input>');
+            
+            // UTM content
+            var utm_content = url.searchParams.get('utm_content');
+            if (utm_content != null && utm_content != 'null')
+                $('#conversion-form').append('<input type="hidden" name="utm_content" value="'+ utm_content +'"></input>');
+	    }, 100);
+    });
+</script>
