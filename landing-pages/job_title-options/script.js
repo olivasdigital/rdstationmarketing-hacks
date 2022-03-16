4a7abd858b3a3ac1833dab8c2943efd2252ab993
Olivas Digital <41170138+olivasdigital@users.noreply.github.com>
2022-03-16 11:07:12 +0100

    Update script.js

--- landing-pages/job_title-options/script.js
+++ landing-pages/job_title-options/script.js
@@ -1,22 +1,25 @@
 <script type="text/javascript">
     $(document).ready(function(){
     	setTimeout(function() {
-		    // Altera o label
-            $("input[name='job_title']").parent().children('label').html('Qual seu cargo?');
+		    // Altera Cargo para select
+		    if( $("input[name='job_title']").length )
+		    {
+			// Altera o label
+			$("input[name='job_title']").parent().children('label').html('Qual sua função ou cargo?');
 
-            // get id
-            var id = $("input[name='job_title']").attr("id");
-
-            // Substitui input text por select
-            $("input[name='job_title']").replaceWith(' <select id="'+ id +'" name="job_title" class="bricks-form__input " data-use-type="STRING">' +
-		          '<option value="">Selecione</option>' +
-		          '<option value="Diretor/Sócio">Diretor/Sócio</option>' +
-		          '<option value="Gerente de Marketing">Gerente de Marketing</option>' +
-		          '<option value="Gerente Financeiro">Gerente Financeiro</option>' +
-		          '<option value="Coordenador ou Analista">Coordenador ou Analista</option>' +
-		          '<option value="Estagiário/Estudante">Estagiário/Estudante</option>' +
-		          '<option value="Outro">Outro</option>' +
-		        '</select>');
+			// Substitui input text por select
+			var id = $("input[name='job_title']").attr("id");
+			$("input[name='job_title']").replaceWith(' <select id="'+ id +'" name="job_title" class="bricks-form__input " data-use-type="STRING">' +
+			    '<option value="">Selecione</option>' +
+			    '<option value="Sócio/diretor">Sócio/diretor</option>' +
+			    '<option value="Gerente Geral">Gerente Geral</option>' +
+			    '<option value="Gerente de Marketing/Comercial">Gerente de Marketing/Comercial</option>' +
+			    '<option value="Coordenador de Marketing">Coordenador de Marketing</option>' +
+			    '<option value="TI">TI</option>' +
+			    '<option value="Compras">Compras</option>' +
+			    '<option value="Outro">Outro</option>' +
+			    '</select>');
+		    }
 	    }, 100);
     });
 </script>
