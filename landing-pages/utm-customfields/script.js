a844e0f402938a975206d534900e9e4e04bf81e6
Olivas Digital <41170138+olivasdigital@users.noreply.github.com>
2022-10-10 13:09:09 +0200

    Update script.js
    Puxa os dados não só do parametro da pagina atual, mas também do cookie de tracking da RD

--- landing-pages/utm-customfields/script.js
+++ landing-pages/utm-customfields/script.js
@@ -1,30 +1,56 @@
 <script type="text/javascript">
-    $(document).ready(function(){
-    	setTimeout(function() {
-		    var endereco_da_pagina = window.location.href;
-            var url = new URL(endereco_da_pagina);
+    // Prepara variaveis
+    var utm_source = "";
+    var utm_medium = "";
+    var utm_campaign = "";
+    var utm_content = "";
+    var params = "";
+    var match = null;
+    var url = "";
+    var data = "";
 
-            // Popula formulário com campos ocultos repassando as UTMs recebidas
-
-            // UTM source
-            var utm_source = url.searchParams.get('utm_source');
-            if (utm_source != null && utm_source != 'null')
-                $('#conversion-form').append('<input type="hidden" name="cf_utm_source" value="'+ utm_source +'"></input>');
+    // Pega os dados do parametro da URL
+    var endereco_da_pagina = window.location.href;
+    
+    // Se a URL não possuir parametros UTM, tenta puxar do cookie tracker do RD Station
+    if( ! endereco_da_pagina.includes("utm_source") )
+    {
+        match = document.cookie.match(new RegExp('(^| )' + '__trf.src' + '=([^;]+)'));
+        if (match){ 
+            data = match[2];
+            if( data.includes("encoded") )
+            {
+                data = data.replace("encoded_", "");
+                data = JSON.parse(atob(data));
+            }
+            else{
+                data = JSON.parse(data);
+            }
             
-            // UTM source
-            var utm_medium = url.searchParams.get('utm_medium');
-            if (utm_medium != null && utm_medium != 'null')
-                $('#conversion-form').append('<input type="hidden" name="cf_utm_medium" value="'+ utm_medium +'"></input>');
+            var current_session = data?.current_session?.value ? data?.current_session?.value : '';
+            var first_session = data?.first_session?.value ? data?.first_session?.value : '';
+            if( current_session !== '' )
+                params = current_session;
+            else if( first_session !== '' )
+                params = first_session;
 
-            // UTM campaign
-            var utm_campaign = url.searchParams.get('utm_campaign');
-            if (utm_campaign != null && utm_campaign != 'null')
-                $('#conversion-form').append('<input type="hidden" name="cf_utm_campaign" value="'+ utm_campaign +'"></input>');
-            
-            // UTM content
-            var utm_content = url.searchParams.get('utm_content');
-            if (utm_content != null && utm_content != 'null')
-                $('#conversion-form').append('<input type="hidden" name="cf_utm_content" value="'+ utm_content +'"></input>');
-	    }, 100);
+            if( endereco_da_pagina.includes('?') )
+                endereco_da_pagina = endereco_da_pagina + '&' + params;
+            else
+                endereco_da_pagina = endereco_da_pagina + '?' + params;
+        }
+    }
+    
+    url = new URL(endereco_da_pagina);
+    utm_source = url.searchParams.get('utm_source') ? url.searchParams.get('utm_source') : '';
+    utm_medium = url.searchParams.get('utm_medium') ? url.searchParams.get('utm_medium') : '';
+    utm_campaign = url.searchParams.get('utm_campaign') ? url.searchParams.get('utm_campaign') : '';
+    utm_content = url.searchParams.get('utm_content') ? url.searchParams.get('utm_content') : '';
+
+    document.querySelectorAll("form").forEach( function(element) {
+        element.insertAdjacentHTML('afterbegin', '<input type="hidden" name="cf_utm_source" value="'+ utm_source +'"></input>');
+        element.insertAdjacentHTML('afterbegin', '<input type="hidden" name="cf_utm_medium" value="'+ utm_medium +'"></input>');
+        element.insertAdjacentHTML('afterbegin', '<input type="hidden" name="cf_utm_campaign" value="'+ utm_campaign +'"></input>');
+        element.insertAdjacentHTML('afterbegin', '<input type="hidden" name="cf_utm_content" value="'+ utm_content +'"></input>');
     });
 </script>
