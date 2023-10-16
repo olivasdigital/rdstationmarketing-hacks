634a26e12cb49a6ac82c9f4c6f34eb5628cae519
Fred Carbonare <fcarbonare@gmail.com>
2023-10-16 09:30:12 +0200

    Create head.js

---
+++ landing-pages/rating-field/head.js
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-bar-rating/1.2.2/themes/bars-square.min.css" integrity="sha512-9iUC/Gxz88/M32cMGOzIJKoYqC+UvYECEkaJ+Th6hVX9CJnDZyQ84Ojc+MUa+HD2+2q4RLTJBiY12IJMdwFFog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
